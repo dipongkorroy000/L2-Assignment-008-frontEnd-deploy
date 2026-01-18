1c1f001ec0338870acb3806c6deafc218af2d91b
dipongkorroy000 <dipongkorroy000@gmail.com>
2026-01-18 21:14:45 +0600

    logout user toast issue fixed

--- src/components/static/LogoutBtn.tsx
+++ src/components/static/LogoutBtn.tsx
@@ -4,15 +4,23 @@ import {logoutUser} from "@/src/services/authentication/logout";
 import {LogOut} from "lucide-react";
 import {Button} from "../ui/button";
 import {toast} from "sonner";
+import {useRouter} from "next/navigation";
 
 const LogoutBtn = () => {
+  const router = useRouter();
+
   const handleLogout = async () => {
-    await logoutUser();
-    toast.success("Logout successfully");
+    try {
+      await logoutUser();
+      toast.success("Logout successfully");
+      router.push("/"); // client-side redirect
+    } catch (_) {
+      toast.error("Logout failed");
+    }
   };
 
   return (
-    <Button variant={"outline"} onClick={handleLogout} className="cursor-pointer border-none w-full text-red-600 hover:text-red-600 hover:bg-red-50">
+    <Button variant="outline" onClick={handleLogout} className="cursor-pointer border-none w-full text-red-600 hover:text-red-600 hover:bg-red-50">
       <LogOut /> Logout
     </Button>
   );
--- src/services/authentication/logout.ts
+++ src/services/authentication/logout.ts
@@ -1,13 +1,13 @@
 "use server";
 
 import {deleteCookie} from "@/src/utils/serverToken";
-import { revalidateTag } from "next/cache";
-import {redirect} from "next/navigation";
+import {revalidateTag} from "next/cache";
+// import {redirect} from "next/navigation";
 
 export const logoutUser = async () => {
   await deleteCookie("accessToken");
   await deleteCookie("refreshToken");
 
-  revalidateTag("user-info", {expire: 0}); 
-  redirect("/");
+  revalidateTag("user-info", {expire: 0});
+  // redirect("/");
 };
